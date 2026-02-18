eab9c954e05cec33ddbbf6bf3c8943d2a9c2c711
Min Khant <minkhant22334456@gmail.com>
2026-02-18 16:30:40 +0630

    Auth Service Changes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -40,13 +40,13 @@ export class AuthService {
   async advertiserLogin(email: string, password: string) {
     try {
       const user = await this.advertiserUserService.findAdvertiserByEmail(email);
-      
-      if(!user?.verified || user.status === 'inactive'){
-        throw new UnauthorizedException('User Not Found!');
-      }
 
       if (!user) {
         throw new UnauthorizedException('Invalid credentials');
+      }
+
+      if(!user?.verified || user.status === 'inactive'){
+        throw new UnauthorizedException('User Not Found!');
       }
 
       const isPasswordValid = await comparePassword(password, user.password);
